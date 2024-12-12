31127f95c6c5f5a2f2911e6721908b04669c987b
Matheus Silva <bastiaotreze@gmail.com>
2024-12-11 22:53:15 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>App.tsx</h1>
+      <p>This is the root component of the app.</p>
+    </>
+  );
+}
+
+export default App;
